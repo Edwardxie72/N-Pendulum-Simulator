e499b994f4533782718ab2f3b05060fd1e312ea2
Edwardxie72 <47434661+Edwardxie72@users.noreply.github.com>
2026-05-11 18:43:48 -0400

    Set up GitHub Actions deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/N-Pendulum-Simulator/',
   plugins: [react()],
+  build: {
+    cssMinify: 'esbuild'
+  }
 })
